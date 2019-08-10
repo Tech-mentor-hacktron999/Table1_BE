f1850fda184f26e16d7a3b624c89da848d2b36a1
AparnaBhogavalli <aparna.cool001@gmail.com>
2019-08-10 12:37:28 +0530

    Modified service logic and Exposing endpoints

--- table1/src/main/java/com/dbs/table1/service/QueueService.java
+++ table1/src/main/java/com/dbs/table1/service/QueueService.java
@@ -2,10 +2,6 @@ package com.dbs.table1.service;
 
 import java.util.ArrayList;
 import java.util.List;
-<<<<<<< HEAD
-=======
-
->>>>>>> 8aca5e234cb6151e6c0aea03e2bb028062a7c692
 import com.dbs.table1.model.Queue;
 
 public class QueueService {
