31b38d957130268d49b932794919d7a618adb9ec
sekhar1104 <sekhar1104@gmail.com>
2019-08-10 12:09:24 +0530

    Queue service delete functionality updated

--- table1/src/main/java/com/dbs/table1/model/Queue.java
+++ table1/src/main/java/com/dbs/table1/model/Queue.java
@@ -13,19 +13,44 @@ public class Queue {
 	private String status;
 	private Date createdAt;
 	private Date updatedAt;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getRecordLimit() {
+		return recordLimit;
+	}
+	public void setRecordLimit(Integer recordLimit) {
+		this.recordLimit = recordLimit;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
 	
 
-	public boolean equals(Object o)
-	{
-		
-		if(o instanceof Queue)
-		{
-			Queue temp = (Queue)o;
-			return this.id.equals(temp.id);
-		}
-		
-		return false;
-		
-		
-	}
 }
