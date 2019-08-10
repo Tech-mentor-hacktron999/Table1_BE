268582d14bff1dae3964f67e8dd74519fd650ba9
AparnaBhogavalli <aparna.cool001@gmail.com>
2019-08-10 13:09:59 +0530

    Adding Test Cases

--- table1/src/test/java/com/dbs/table1/controller/MessageControllerTest.java
+++ table1/src/test/java/com/dbs/table1/controller/MessageControllerTest.java
@@ -1,5 +1,38 @@
 package com.dbs.table1.controller;
 
-public class MessageControllerTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import com.dbs.table1.model.Message;
+import com.dbs.table1.service.MessageService;
+import com.dbs.table1.service.QueueService;
+
+public class MessageControllerTest {
+	
+	private MessageService messageService;
+	
+	@Before
+	public void setUp(){
+		messageService = new MessageService();	
+	}
+	@Test
+	public void addMessageTest(String queueId, Message message){
+		
+	}
+	
+	@Test
+	public void deleteMessageTest(String queueId){
+		
+	}
+	
+	@Test
+	public void fetchMessageTest(String queueId){
+		
+	}
+	
+	@After
+	public void tearDown(){
+		messageService = null;
+	}
 }
